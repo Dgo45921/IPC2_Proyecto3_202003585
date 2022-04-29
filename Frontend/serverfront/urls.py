cbe10a9d57163bb17bd93d35ec7d529311d62b37
Dgo45921 <3023135080101@ingenieria.edu.gt>
2022-04-29 13:50:17 -0600

    empezando el frontend

--- Frontend/serverfront/urls.py
+++ Frontend/serverfront/urls.py
@@ -1,4 +1,4 @@
-"""frontend URL Configuration
+"""serverfront URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from sitioweb import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.home)
+
 ]
