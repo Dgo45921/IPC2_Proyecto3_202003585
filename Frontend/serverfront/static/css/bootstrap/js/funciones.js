82793c7115fdaad1153951fbb452e2ba4bd42271
Dgo45921 <3023135080101@ingenieria.edu.gt>
2022-05-04 13:52:04 -0600

    funcionalidad pdf

--- Frontend/serverfront/static/css/bootstrap/js/funciones.js
+++ Frontend/serverfront/static/css/bootstrap/js/funciones.js
@@ -1,11 +1,6 @@
 document.getElementById('get_file').onclick = function (){
     document.getElementById('my_file').click();
 }
-// para generar el pdf del rango de fechas
-document.getElementById('reporte_rango').onclick = function (){
-    document.getElementById('boton_rango').click();
-}
-
 
 function cargarXML(event){
     archivo = event.target.files[0];
@@ -34,15 +29,51 @@ function genera_pdf_rango_fechas(){
 }
 
 function genera_pdf_fecha(){
-    console.log("hola")
  var ctxbar = document.getElementById("grafica")
     let imgcanvas = ctxbar.toDataURL("image/png", 1.0);
 
 
     var doc = new jsPDF('landscape')
-    doc.text("REPORTE CLASIFICACION POR FECHA", 120, 20)
+    doc.text("REPORTE CLASIFICACION POR FECHA", 90, 20)
     doc.addImage(imgcanvas, 'JPEG', 20, 25, 250, 180)
 
     doc.save("Reporte_fecha")
 
+}
+
+function genera_pdf_entrada_salida(){
+    var input = document.getElementById('input').value
+    var output = document.getElementById('area_salida').value
+    var doc = new jsPDF('p', 'mm', [700, 8000]);
+    doc.text("TEXTO ENTRADA", 80, 10)
+    doc.setFontSize(10);
+    doc.text(input,0,20)
+    doc.setFontSize(1);
+
+    doc.addPage();
+    doc.setFontSize(10);
+    doc.text("TEXTO SALIDA", 80, 10)
+    doc.setFontSize(10);
+    doc.text(output,0,30)
+    doc.setFontSize(1);
+    doc.save("Reporte_entrada_salida")
+}
+
+
+function genera_pdf_entrada_salida_prueba(){
+    var input = document.getElementById('input').value
+    var output = document.getElementById('area_salida').value
+    var doc = new jsPDF('p', 'mm', [700, 1000]);
+    doc.text("TEXTO ENTRADA", 80, 10)
+    doc.setFontSize(10);
+    doc.text(input,0,20)
+    doc.setFontSize(1);
+
+    doc.addPage();
+    doc.setFontSize(10);
+    doc.text("TEXTO SALIDA", 80, 10)
+    doc.setFontSize(10);
+    doc.text(output,0,30)
+    doc.setFontSize(1);
+    doc.save("Reporte_entrada_salida")
 }
