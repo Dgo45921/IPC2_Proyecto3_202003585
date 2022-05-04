13561666a20672f28a9b359234352cb5e49519b2
Dgo45921 <3023135080101@ingenieria.edu.gt>
2022-05-04 12:42:58 -0600

    avance en pdf

--- Frontend/serverfront/static/css/bootstrap/js/funciones.js
+++ Frontend/serverfront/static/css/bootstrap/js/funciones.js
@@ -1,6 +1,11 @@
 document.getElementById('get_file').onclick = function (){
     document.getElementById('my_file').click();
 }
+// para generar el pdf del rango de fechas
+document.getElementById('reporte_rango').onclick = function (){
+    document.getElementById('boton_rango').click();
+}
+
 
 function cargarXML(event){
     archivo = event.target.files[0];
@@ -13,4 +18,31 @@ function cargarXML(event){
         area_entrada.value = contenido_xml
    }
     fr.readAsText(archivo)
+}
+
+function genera_pdf_rango_fechas(){
+ var ctxbar = document.getElementById("grafica")
+    let imgcanvas = ctxbar.toDataURL("image/png", 1.0);
+
+
+    var doc = new jsPDF('landscape')
+    doc.text("REPORTE RANGO FECHAS", 120, 20)
+    doc.addImage(imgcanvas, 'JPEG', 20, 25, 250, 180)
+
+    doc.save("Reporte_rango_fechas")
+
+}
+
+function genera_pdf_fecha(){
+    console.log("hola")
+ var ctxbar = document.getElementById("grafica")
+    let imgcanvas = ctxbar.toDataURL("image/png", 1.0);
+
+
+    var doc = new jsPDF('landscape')
+    doc.text("REPORTE CLASIFICACION POR FECHA", 120, 20)
+    doc.addImage(imgcanvas, 'JPEG', 20, 25, 250, 180)
+
+    doc.save("Reporte_fecha")
+
 }
